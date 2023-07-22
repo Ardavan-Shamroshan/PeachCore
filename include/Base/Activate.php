db1966b82e7f577221d83df204ede0e290856bce
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-07-22 23:39:01 +0330

    cpt tabs, table implemented

--- include/Base/Activate.php
+++ include/Base/Activate.php
@@ -10,10 +10,13 @@ class Activate {
 	public static function activate() {
 		flush_rewrite_rules();
 
-		if ( get_option( 'peach_core_plugin' ) ) {
-			return;
+		// if there was no option with the given key, update the key with an empty array
+		if ( ! get_option( 'peach_core_plugin' ) ) {
+			update_option( 'peach_core_plugin', [] );
 		}
 
-		update_option( 'peach_core_plugin', [] );
+		if ( ! get_option( 'peach_core_plugin_cpt' ) ) {
+			update_option( 'peach_core_plugin_cpt', [] );
+		}
 	}
 }
--- templates/custom-post-type.php
+++ templates/custom-post-type.php
@@ -2,12 +2,55 @@
     <h1>نوع پست اختصاصی</h1>
 	<?php settings_errors(); ?>
 
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#tab-1">لیست انواع پست های اختصاصی</a></li>
+        <li><a href="#tab-2">ایجاد نوع پست اختصاصی</a></li>
+        <li><a href="#tab-3">استخراج</a></li>
+    </ul>
 
-    <form action="options.php" method="POST">
-		<?php
-		settings_fields( 'peach_core_plugin_cpt_settings' );
-		do_settings_sections( 'peach-core-custom-post-type-submenu' );
-		submit_button();
-		?>
-    </form>
+    <div class="tab-content">
+        <div id="tab-1" class="tab-pane active">
+            <h3>لیست پست ها</h3>
+			<?php $options = get_option( 'peach_core_plugin_cpt' ); ?>
+
+            <table class="cpt-table">
+                <tr>
+                    <th>شناسه</th>
+                    <th>نام</th>
+                    <th>نام مفرد</th>
+                    <th>عمومی</th>
+                    <th>دارای آرشیو</th>
+                    <th class="text-center">عملیات</th>
+                </tr>
+                <tbody>
+				<?php foreach ( $options as $option ): ?>
+                    <tr>
+                        <td><?= $option['post_type'] ?></td>
+                        <td><?= $option['name'] ?></td>
+                        <td><?= $option['singular_name'] ?></td>
+                        <td><?= $option['public'] ?></td>
+                        <td><?= $option['has_archive'] ?></td>
+                        <td class="text-center"><a href="#">ویرایش</a> - <a href="#">حذف</a></td>
+                    </tr>
+				<?php endforeach; ?>
+                </tbody>
+            </table>
+
+        </div>
+
+        <div id="tab-2" class="tab-pane">
+
+            <form action="options.php" method="POST">
+				<?php
+				settings_fields( 'peach_core_plugin_cpt_settings' );
+				do_settings_sections( 'peach-core-custom-post-type-submenu' );
+				submit_button();
+				?>
+            </form>
+        </div>
+        <div id="tab-3" class="tab-pane">
+            <h3>استخراج</h3>
+        </div>
+    </div>
+
 </div>
