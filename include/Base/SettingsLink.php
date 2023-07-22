0a969db72712782d0c3318352b38ecfd266decad
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2023-07-22 18:34:33 +0330

    settings-link of plugin modified

--- include/Base/SettingsLink.php
+++ include/Base/SettingsLink.php
@@ -3,17 +3,12 @@
 namespace Inc\Base;
 
 class SettingsLink extends BaseController {
-//	protected $plugin;
-//	public function __construct() {
-//		$this->plugin = ARDAVAN;
-//	}
-
 	public function register() {
 		add_filter("plugin_action_links_$this->plugin", [$this, 'settings_link']);
 	}
 
 	public function settings_link( $links ) {
-		$settings_link = "<a href='admin.php?page=peach-core-plugin'>Settings</a>";
+		$settings_link = "<a href='admin.php?page=peach-core'>تنظیمات</a>";
 		$links[]       = $settings_link;
 
 		return $links;
